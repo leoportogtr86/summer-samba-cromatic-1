31456bdd2033d90c65fff30f03dc8f88fc30e535
Leo Porto <leonardoreisporto@gmail.com>
2020-07-02 06:09:42 -0300

    Updates

---
+++ precache-manifest.27b038d27f176629b5b88efaf596fc0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa6be4a2baaccbbf41204247356fc946",
+    "url": "/summer-samba-cromatic-1/index.html"
+  },
+  {
+    "revision": "497fef58fe13a61a8054",
+    "url": "/summer-samba-cromatic-1/static/css/main.f7a160da.chunk.css"
+  },
+  {
+    "revision": "09410e69275b35fe76a2",
+    "url": "/summer-samba-cromatic-1/static/js/2.6617b313.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/summer-samba-cromatic-1/static/js/2.6617b313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "497fef58fe13a61a8054",
+    "url": "/summer-samba-cromatic-1/static/js/main.757c786a.chunk.js"
+  },
+  {
+    "revision": "5c58944ad34b6d1a6cd9",
+    "url": "/summer-samba-cromatic-1/static/js/runtime-main.97c64a4d.js"
+  }
+]);
